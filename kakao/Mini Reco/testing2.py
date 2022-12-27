f0f91324262ff5b07522326310b23c79fec7fe0e
HwangHanJae <han8018@gmail.com>
2022-12-28 00:22:01 +0900

    테스트케이스 자동 입력시 모듈화

--- kakao/Mini Reco/testing2.py
+++ kakao/Mini Reco/testing2.py
@@ -1,50 +1,17 @@
 import os
 from evaluation import ndcg
 from solution import UserBaseCF
+from Case import DataFrame_Matrix
 import pandas as pd
 number = str(4)
-base_input_path = os.getcwd()+'\\kakao\Mini Reco\\testcase\input'
-base_output_path = os.getcwd()+'\\kakao\Mini Reco\\testcase\output'
-input_file = f'input00{number}.txt'
-output_file = f'output00{number}.txt'
-
-#input
-input_path = os.path.join(base_input_path, input_file)
-f = open(input_path, 'r')
-lines = f.readlines()
-num_sim_user_top_N = int(lines[0].strip())
-num_item_rec_top_N = int(lines[1].strip())
-num_users = int(lines[2].strip())
-num_items = int(lines[3].strip())
-num_rows  = int(lines[4].strip())
-
-#matrix 초기화
-matrix = pd.DataFrame()
-
-for line in lines[5:num_rows+5]:
-  data = line.strip().split()
-  u = int(data[0])
-  i = int(data[1])
-  rating = float(data[2])
-  matrix.loc[u, i] = rating
-  
-num_reco_users = lines[num_rows+5]
-active_users = []
-for line in lines[num_rows+6:]:
-  user = int(line.strip())
-  active_users.append(user)
-  
-f.close()
-# output
-output_path = os.path.join(base_output_path, output_file)
-f = open(output_path, 'r')
-lines = f.readlines()
-gts = []
-for line in lines:
-  gt = list(map(int,line.strip().split()))
-  gts.append(gt)
-f.close()
-
+testcase = DataFrame_Matrix(number)
+matrix = testcase.matrix
+num_users = testcase.num_users
+num_items = testcase.num_items
+num_sim_user_top_N = testcase.num_sim_user_top_N
+num_item_rec_top_N = testcase.num_item_rec_top_N
+active_users = testcase.active_users
+gts = testcase.gts
 #solve
 model =UserBaseCF(matrix, num_users, num_items, num_sim_user_top_N, num_item_rec_top_N)
 scores = []
